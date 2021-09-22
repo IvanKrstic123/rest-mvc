14bf488033b3bec9f5514f05a9e36cd2b1ea19a2
Ivan Krstic <krstic8687@yahoo.com>
2021-09-23 00:39:59 +0200

    Setting Up MapStruct for Type Conversion **

---
+++ src/test/java/guru/springframework/restmvc/api/v1/mapper/CategoryMapperTest.java
@@ -0,0 +1,37 @@
+package guru.springframework.restmvc.api.v1.mapper;
+
+import guru.springframework.restmvc.api.v1.model.CategoryDTO;
+import guru.springframework.restmvc.domain.Category;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryMapperTest {
+
+    public static final long ID = 1L;
+    public static final String SWEETS = "Sweets";
+
+    CategoryMapper categoryMapper = CategoryMapper.INSTANCE;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void categoryToCategoryDTO() {
+
+        //given
+        Category category = new Category();
+        category.setId(ID);
+        category.setName(SWEETS);
+
+        //when
+        CategoryDTO categoryDTO = categoryMapper.categoryToCategoryDTO(category);
+
+        //then
+        assertNotNull(categoryDTO);
+        assertEquals(Long.valueOf(1), categoryDTO.getId());
+        assertEquals("Sweets", categoryDTO.getName());
+    }
+}
