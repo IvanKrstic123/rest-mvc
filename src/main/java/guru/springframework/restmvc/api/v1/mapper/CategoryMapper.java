f267e035e527dc2b87f61fb01dd8cac26e78eb92
Ivan Krstic <krstic8687@yahoo.com>
2021-09-23 00:57:49 +0200

    Testing MapStructSupport Plugin

--- src/main/java/guru/springframework/restmvc/api/v1/mapper/CategoryMapper.java
+++ src/main/java/guru/springframework/restmvc/api/v1/mapper/CategoryMapper.java
@@ -3,6 +3,7 @@ package guru.springframework.restmvc.api.v1.mapper;
 import guru.springframework.restmvc.api.v1.model.CategoryDTO;
 import guru.springframework.restmvc.domain.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -10,5 +11,6 @@ public interface CategoryMapper {
 
     CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
 
+    @Mapping(source = "id", target = "id")
     CategoryDTO categoryToCategoryDTO(Category category);
 }
