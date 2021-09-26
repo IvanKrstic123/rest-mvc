5508a9be96ddb7eca2b6ebaa06a5baee5985f299
Ivan Krstic <krstic8687@yahoo.com>
2021-09-26 17:54:22 +0200

    Integration Testing With DataJpaTest

---
+++ src/test/java/guru/springframework/restmvc/services/CustomerServiceImplIT.java
@@ -0,0 +1,77 @@
+package guru.springframework.restmvc.services;
+
+import guru.springframework.restmvc.api.v1.mapper.CustomerMapper;
+import guru.springframework.restmvc.api.v1.model.CustomerDTO;
+import guru.springframework.restmvc.bootstrap.Bootstrap;
+import guru.springframework.restmvc.domain.Customer;
+import guru.springframework.restmvc.repositories.CategoryRepository;
+import guru.springframework.restmvc.repositories.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.not;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+public class CustomerServiceImplIT {
+
+    public static final long ID = 1L;
+    public static final String FIRSTNAME = "Fred";
+    public static final String LASTNAME = "Mayers";
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    private CustomerService customerService;
+
+
+    @BeforeEach
+    void setUp() throws Exception {
+        Bootstrap bootstrap = new Bootstrap(categoryRepository, customerRepository);
+        bootstrap.run(); // load data
+
+        customerService = new CustomerServiceImpl(customerRepository, CustomerMapper.INSTANCE);
+    }
+
+    @Test
+    void patchCustomerUpdateFirstname() {
+        //given
+        Customer existingCustomer = customerRepository.findAll().get(0);
+
+        CustomerDTO requestDTO = new CustomerDTO();
+        requestDTO.setFirstname(FIRSTNAME);
+
+        CustomerDTO returnedDTO = customerService.patchCustomer(existingCustomer.getId(), requestDTO);
+
+        assertEquals(existingCustomer.getId(), returnedDTO.getId());
+        assertEquals(FIRSTNAME, returnedDTO.getFirstname());
+        assertEquals(existingCustomer.getLastname(), returnedDTO.getLastname());
+        assertEquals("/api/v1/customers/" + existingCustomer.getId(), returnedDTO.getCustomerUrl());
+
+    }
+
+    @Test
+    void patchCustomerUpdateLastname() {
+        //given
+        Customer existingCustomer = customerRepository.findAll().get(0);
+
+        CustomerDTO requestDTO = new CustomerDTO();
+        requestDTO.setLastname(LASTNAME);
+
+        CustomerDTO returnedDTO = customerService.patchCustomer(existingCustomer.getId(), requestDTO);
+
+        assertNotNull(existingCustomer);
+        assertEquals(existingCustomer.getId(), returnedDTO.getId());
+        assertEquals(LASTNAME, returnedDTO.getLastname());
+        assertEquals(existingCustomer.getLastname(), returnedDTO.getLastname());
+        assertEquals("/api/v1/customers/" + existingCustomer.getId(), returnedDTO.getCustomerUrl());
+    }
+}
